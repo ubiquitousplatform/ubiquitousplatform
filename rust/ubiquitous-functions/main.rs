c2b5e15c7991045cb218ad83d83c36c143c79ab0
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2023-12-29 14:13:23 -0800

    Add sample ubiq.func project

---
+++ rust/ubiquitous-functions/main.rs
@@ -0,0 +1,24 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+use anyhow::Result;
+use wasmtime::*;
+
+struct MyState {
+    name: String,
+    count: usize,
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
