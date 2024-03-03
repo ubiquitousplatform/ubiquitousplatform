642be5fb5df1aafa08d1ea48214843584b1c8451
ubiquitous-dev <ubiquitousobjects@protonmail.com>
2024-03-02 16:46:08 -0800

    comment out unworking wrapper attempt

--- test-harness/src/index.ts
+++ test-harness/src/index.ts
@@ -1,4 +1,5 @@
 import { distance, closest } from "fastest-levenshtein";
+import { withExtism } from "./withExtism";
 
 // this function is private to the module
 function privateFunc() {
@@ -13,10 +14,18 @@ function asExtismFunction(fn: Function) {
   };
 }
 
-export var get_closest: () => void = asExtismFunction(
+export var get_closest = asExtismFunction(
   (input: string) =>
     `${closest(input, ["slow", "faster", "fastest"])} ${privateFunc()}`
 );
+
+// the downside to this type of wrapping is that it happens at runtime. also it doesn't seem to work right.
+/*
+export var get_closest = () =>
+  withExtism(
+    async (input: string) =>
+      `${closest(input, ["slow", "faster", "fastest"])} ${privateFunc()}`
+  );*/
 
 /*
 // use any export syntax to export a function be callable by the extism host
---
+++ test-harness/src/withExtism.ts
@@ -0,0 +1,16 @@
+export function withExtism(func: (...params: any[]) => void) {
+  (async () => {
+    let input = [];
+    try {
+      input = JSON.parse(Host.inputString());
+    } catch (e) {
+      console.error(
+        "\nERROR: Function input not JSON formatted, unable to parse!",
+        e
+      );
+      return -1;
+    }
+    const resp = await func(...input);
+    Host.outputString(JSON.stringify(resp));
+  })();
+}
